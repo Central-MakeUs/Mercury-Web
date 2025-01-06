1a6ce759a703a7a395337f2acc2815e3e89355cd
YuGilJong <101111364+XionWCFM@users.noreply.github.com>
2025-01-06 23:33:32 +0900

    feat: 리액트 라우터 디펜던시 추가, 라우팅 구조 설계 (#11)
    * feat: add dependencies react-router
    
    * feat: react-router 연동 및 라우팅 구조 설계
    
    * refactor: 피드백 반영하여 reading-record -

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,30 @@ import { worker } from "@repo/mocks/browser";
 import { Providers } from "@repo/providers";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import BottomNavigationLayout from "./app/BottomNavigationLayout.tsx";
+import BookRecordPage from "./pages/BookRecordPage.tsx";
+import BookRecordWritePage from "./pages/BookRecordWritePage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import OnBoardingPage from "./pages/OnBoardingPage.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
+import TimerPage from "./pages/TimerPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Providers>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<BottomNavigationLayout />}>
+            <Route path="home" element={<HomePage />} />
+            <Route path="timer" element={<TimerPage />} />
+            <Route path="book-record" element={<BookRecordPage />} />
+            <Route path="profile" element={<ProfilePage />} />
+          </Route>
+          <Route path="book-record/write" element={<BookRecordWritePage />} />
+          <Route path="" element={<OnBoardingPage />} />
+        </Routes>
+      </BrowserRouter>
       <Analytics />
     </Providers>
   </StrictMode>,
