bfed940442c01b6946df15db9f8071950348f2db
devgiljong <calf-love@naver.com>
2025-01-07 19:48:43 +0900

    feat: text 컴포넌트 구현

--- packages/design-system/Text.tsx
+++ packages/design-system/Text.tsx
@@ -4,17 +4,30 @@ import { type VariantProps, cva } from "class-variance-authority";
 import { type ElementType, forwardRef } from "react";
 import { cn } from "./cn";
 
-export const textVariants = cva("", {
+export const textVariants = cva("leading-[150%] tracking-[-2%]", {
   variants: {
     variant: {
-      title25b: "",
+      "title/25_b": "font-bold text-[25px]",
+      "title/25_sb": "font-semibold text-[25px]",
+      "title/25_m": "font-medium text-[25px]",
+      "title/24_sb": "font-semibold text-[24px]",
+      "title/24_m": "font-medium text-[24px]",
+      "title/20_b": "font-bold text-[20px]",
+      "title/20_sb": "font-semibold text-[20px]",
+      "title/16_b": "font-bold text-[16px]",
+      "title/16_sb": "font-semibold text-[16px]",
+      "title/16_m": "font-medium text-[16px]",
+      "title/14_sb": "font-semibold text-[14px]",
+      "title/14_m": "font-medium text-[14px]",
+      "title/12_sb": "font-semibold text-[12px]",
+      "title/12_m": "font-medium text-[12px]",
+      "title/12_r": "font-normal text-[12px]",
     },
   },
 });
-
 type TextProps = VariantProps<typeof textVariants>;
 
-export const Text = forwardRef(function Text<C extends ElementType = "div">(
+export const Text = forwardRef(function Text<C extends ElementType = "span">(
   { as, className, variant, ...rest }: BoxProps<C> & TextProps,
   ref?: BoxRef<C>,
 ) {
