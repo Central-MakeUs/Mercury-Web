bae343cef816fe79c28b2bff3273cf87b1b335b1
devgiljong <calf-love@naver.com>
2025-01-08 19:53:04 +0900

    feat: 프로젝트에 연동

--- src/app/BottomNavigationLayout.tsx
+++ src/app/BottomNavigationLayout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router";
+import { MercuryBottomNavigationBar } from "~/widgets/MercuryBottomNavigationBar";
 
 export default function BottomNavigationLayout() {
   return (
     <>
       <Outlet />
-      <div>bottomnavigation</div>
+      <MercuryBottomNavigationBar />
     </>
   );
 }
