c562c77af859cab94860f72b28eddcafafa66d5b
devgiljong <calf-love@naver.com>
2025-01-15 00:36:29 +0900

    feat: time speect bubble implement

---
+++ src/features/timer/components/TimerSpeechBubble.tsx
@@ -0,0 +1,26 @@
+import { AspectRatio } from "@repo/design-system/AspectRatio";
+import { Image } from "@repo/design-system/Image";
+import { Text } from "@repo/design-system/Text";
+import { cn } from "@repo/design-system/cn";
+import { Box } from "@repo/ui/Box";
+import type { ComponentPropsWithoutRef } from "react";
+import { TIMER_ASSETS } from "../lib/timer.constants";
+
+interface TimerSpeechBubbleProps extends ComponentPropsWithoutRef<"div"> {}
+
+export const TimerSpeechBubble = (props: TimerSpeechBubbleProps) => {
+  const { children, className, ...rest } = props;
+  return (
+    <Box className={cn(" relative h-[60px]", className)} {...rest}>
+      <AspectRatio className="w-[186px] h-[60px]" ratio={186 / 60}>
+        <Image src={TIMER_ASSETS.SPEECH_BUBBLE_BG} alt="timer speech bubble background image" />
+      </AspectRatio>
+      <Text
+        variant={"body/16_sb"}
+        className=" absolute top-0 left-0 w-[186px] h-[42px] flex justify-center items-center text-yellow-green"
+      >
+        {children}
+      </Text>
+    </Box>
+  );
+};
