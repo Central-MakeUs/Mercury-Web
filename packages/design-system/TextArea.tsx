9fe2e95b8bcb9068d5248c4e341294a5d2865c7c
Youjiiin <yujinjang24@gmail.com>
2025-01-15 23:18:48 +0900

    fix: 텍스트 area 컴포넌트 분리 및 react hook form 적용

--- packages/design-system/TextArea.tsx
+++ packages/design-system/TextArea.tsx
@@ -1,33 +1,23 @@
-import { useState } from "react";
-import { MaxWidthBox } from "./MaxWidthBox";
-import { Text, textVariants } from "./Text";
+import { type Control, useWatch } from "react-hook-form";
+import { Text } from "./Text";
 import { cn } from "./cn";
 
-export const TextArea = () => {
-  const [text, setText] = useState("");
-
+export const LetterCount = (props: {
+  control: Control<{
+    text: string;
+  }>;
+  name: "text";
+}) => {
+  const watch = useWatch({ control: props.control, name: props.name }) || "";
   return (
-    <MaxWidthBox className="w-full min-h-[343px] relative">
-      <textarea
-        placeholder="내용을 입력해주세요"
-        maxLength={1000}
-        wrap="hard"
-        required={true}
-        onChange={(e) => setText(e.target.value)}
-        className={cn(
-          textVariants({ variant: "title/16_m" }),
-          "w-full h-full min-h-[343px] bg-yellow-green py-[10px] px-[15px] rounded-3 text-gray-600 placeholder:gray-400",
-        )}
-      />
-      <Text
-        variant="body/14_m"
-        className={cn(
-          "absolute bottom-[9px] right-[14px] text-gray-300",
-          text.length > 0 && "text-gray-500",
-        )}
-      >
-        {text.length}
-      </Text>
-    </MaxWidthBox>
+    <Text
+      variant="body/14_m"
+      className={cn(
+        "absolute bottom-[29px] right-[30px] text-gray-300",
+        watch.length > 0 && "text-gray-500",
+      )}
+    >
+      {watch.length}
+    </Text>
   );
 };
