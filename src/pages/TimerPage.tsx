5a194faa29c8ac777e2b7847a8822051336f12ab
devgiljong <calf-love@naver.com>
2025-01-18 22:52:43 +0900

    refactor: 타이머 정렬논리 수정

--- src/pages/TimerPage.tsx
+++ src/pages/TimerPage.tsx
@@ -1,4 +1,5 @@
 import { AspectRatio } from "@repo/design-system/AspectRatio";
+import { BottomNavigationBar } from "@repo/design-system/BottomNavigationBar";
 import { Button } from "@repo/design-system/Button";
 import { Image } from "@repo/design-system/Image";
 import { Text } from "@repo/design-system/Text";
@@ -12,34 +13,38 @@ import { TIMER_STATUS } from "~/features/timer/model/timer.model";
 
 export default function TimerPage() {
   return (
-    <Stack className=" pt-[24px] w-full">
-      <Text variant={"title/24_sb"} className=" px-[20px] text-gray-800 mb-[26px]">
-        타이머
-      </Text>
-
-      <CenterStack>
-        <TimerSpeechBubble className=" mb-[2px]">
-          {getTimerSpeechBubbleText(TIMER_STATUS.INIT)}
-        </TimerSpeechBubble>
-      </CenterStack>
-
-      <CenterStack className=" w-full px-[46px]">
-        <AspectRatio className=" w-full" ratio={1}>
-          <Image src={TIMER_ASSETS.CHARACTER} alt="timer character" />
-        </AspectRatio>
-      </CenterStack>
-
-      <CenterStack className=" text-center my-[32px]">
-        <Text variant={"body/18_m"} className=" text-gray-1000 whitespace-pre-wrap">
-          {getTimerDescription(TIMER_STATUS.INIT)}
+    <Stack className=" pt-[24px] justify-between w-full ">
+      <Stack>
+        <Text variant={"title/24_sb"} className=" px-[20px] text-gray-800 mb-[26px]">
+          타이머
         </Text>
-      </CenterStack>
 
-      <CenterStack>
-        <Button size="medium" variant="primary">
-          시간 설정하기
-        </Button>
-      </CenterStack>
+        <CenterStack>
+          <TimerSpeechBubble className=" mb-[2px]">
+            {getTimerSpeechBubbleText(TIMER_STATUS.INIT)}
+          </TimerSpeechBubble>
+        </CenterStack>
+
+        <CenterStack className=" w-full px-[46px]">
+          <AspectRatio className=" w-full" ratio={1}>
+            <Image src={TIMER_ASSETS.CHARACTER} alt="timer character" />
+          </AspectRatio>
+        </CenterStack>
+      </Stack>
+
+      <Stack>
+        <CenterStack className=" text-center my-[32px]">
+          <Text variant={"body/18_m"} className=" text-gray-1000 whitespace-pre-wrap">
+            {getTimerDescription(TIMER_STATUS.INIT)}
+          </Text>
+        </CenterStack>
+        <CenterStack>
+          <Button size="medium" variant="primary">
+            시간 설정하기
+          </Button>
+        </CenterStack>
+        <BottomNavigationBar.Height className=" mb-[62.5px]" />
+      </Stack>
     </Stack>
   );
 }
