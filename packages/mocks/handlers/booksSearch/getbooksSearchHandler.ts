cf96b7069415cd16881d5e91991d35e2598bc87a
devgiljong <calf-love@naver.com>
2025-01-19 18:09:54 +0900

    feat: mock 구현세부화

--- packages/mocks/handlers/booksSearch/getbooksSearchHandler.ts
+++ packages/mocks/handlers/booksSearch/getbooksSearchHandler.ts
@@ -1,9 +1,19 @@
 import { http, HttpResponse } from "msw";
+import type { GetBooksSearchSortType } from "~/entities/record/api/getBooksSearch";
 import { baseUrl } from "../../constants";
 import { mockCreateGetBooksSearchResponse } from "./getBooksSearch.mock";
 
 export const getBooksSearchHandler = [
-  http.get(`${baseUrl}/books/search`, () => {
-    return HttpResponse.json(mockCreateGetBooksSearchResponse());
+  http.get(`${baseUrl}/books/search`, ({ request }) => {
+    const url = new URL(request.url);
+
+    const query = url.searchParams.get("query") ?? "";
+    const maxResults = Number(url.searchParams.get("maxResults")) ?? 10;
+    const sortType = url.searchParams.get("sortType") as GetBooksSearchSortType;
+    const startPage = Number(url.searchParams.get("startPage")) ?? 1;
+
+    return HttpResponse.json(
+      mockCreateGetBooksSearchResponse({ query, maxResults, sortType, startPage }),
+    );
   }),
 ];
