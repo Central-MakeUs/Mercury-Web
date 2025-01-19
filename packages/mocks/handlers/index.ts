41fd88dd797e715f61310ff219616a4c263524ba
devgiljong <calf-love@naver.com>
2025-01-19 18:03:12 +0900

    chore: remove dead code

--- packages/mocks/handlers/index.ts
+++ packages/mocks/handlers/index.ts
@@ -1,5 +1,4 @@
 import { getBooksSearchHandler } from "./booksSearch/getbooksSearchHandler";
-import { helloHandler } from "./hello/helloHandler";
 import { mockHandler } from "./mock/mockHandler";
 
-export const handlers = [...helloHandler, ...mockHandler, ...getBooksSearchHandler];
+export const handlers = [...mockHandler, ...getBooksSearchHandler];
