750a2ba9fbd36239e1c4f815880ff5e7c5764b11
Youjiiin <yujinjang24@gmail.com>
2025-01-21 01:19:42 +0900

    feat: 드롭다운 컴포넌트

---
+++ src/features/bookRecordWrite/components/DropDownMenu.tsx
@@ -0,0 +1,48 @@
+import * as Dropdown from "@radix-ui/react-dropdown-menu";
+import { textVariants } from "@repo/design-system/Text";
+import { cn } from "@repo/design-system/cn";
+import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from "react";
+
+export const Root = Dropdown.Root;
+export const Trigger = Dropdown.Trigger;
+export const Portal = Dropdown.Portal;
+
+export const Content = forwardRef<
+  ElementRef<typeof Dropdown.Content>,
+  ComponentPropsWithoutRef<typeof Dropdown.Content>
+>(({ className, ...props }, ref) => (
+  <Dropdown.Content
+    ref={ref}
+    className={cn("bg-white rounded-[14px] shadow-lg p-2 z-[9999]", className)}
+    align="end"
+    side="bottom"
+    sideOffset={8}
+    {...props}
+  />
+));
+Content.displayName = "DropdownContent";
+
+export const Item = forwardRef<
+  ElementRef<typeof Dropdown.Item>,
+  ComponentPropsWithoutRef<typeof Dropdown.Item> & { onClick?: () => void }
+>(({ className, onClick, ...props }, ref) => (
+  <Dropdown.Item
+    ref={ref}
+    onClick={onClick}
+    className={cn(
+      "px-4 py-[8.5px] text-gray-600 cursor-pointer mr-[25px]",
+      textVariants({ variant: "body/15_m" }),
+      className,
+    )}
+    {...props}
+  />
+));
+Item.displayName = "DropdownItem";
+
+export const DropdownMenu = {
+  Root,
+  Trigger,
+  Portal,
+  Content,
+  Item,
+};
