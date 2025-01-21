9054a3c3235e8a20573b82a0878f9c4554b35c14
Yujin <114130942+Youjiiin@users.noreply.github.com>
2025-01-22 01:19:16 +0900

    feat: dialog (#52)
    * feat: 다이얼로그 컨테이너
    
    * feat: 다이얼로그 confrim content
    
    * feat: 다이얼로그 menu component
    
    * fix: 파일명 변경
    
    * fix: 다이얼로그 코드스타일 변경
    
    * fix: 다이얼로그 컴포넌트 명 변경

---
+++ src/features/bookRecordDetail/components/DialogConfirm.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@repo/design-system/Button";
+import { textVariants } from "@repo/design-system/Text";
+import { cn } from "@repo/design-system/cn";
+import { Flex } from "@repo/ui/Flex";
+import { Stack } from "@repo/ui/Stack";
+import { DialogContent } from "./DialogContent";
+
+interface DialogConfirmProps {
+  onClose: () => void;
+}
+
+export const DialogConfirm = ({ onClose }: DialogConfirmProps) => {
+  return (
+    <DialogContent.Content>
+      <Stack className="p-4 gap-[35px]">
+        <DialogContent.Title
+          className={cn(
+            "DialogTitle text-center mt-[25px] text-gray-800",
+            textVariants({ variant: "title/20_sb" }),
+          )}
+        >
+          모든 메모가 한 번에 삭제돼요
+          <br /> 정말 삭제할까요?
+        </DialogContent.Title>
+
+        <Flex className="gap-2">
+          <Button
+            onClick={onClose}
+            className="rounded-[14px] bg-gray-200 text-gray-400 max-w-[56px] flex items-center justify-center"
+          >
+            아니요
+          </Button>
+          <Button
+            onClick={() => {
+              alert("삭제되었습니다.");
+              onClose();
+            }}
+            className="rounded-[14px] bg-warning-red text-white max-w-[56px] flex items-center justify-center"
+          >
+            삭제하기
+          </Button>
+        </Flex>
+      </Stack>
+    </DialogContent.Content>
+  );
+};
---
+++ src/features/bookRecordDetail/components/DialogMenu.tsx
@@ -0,0 +1,31 @@
+import { textVariants } from "@repo/design-system/Text";
+import { cn } from "@repo/design-system/cn";
+import { Stack } from "@repo/ui/Stack";
+import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from "react";
+import { DialogContent } from "./DialogContent";
+
+export const Item = forwardRef<ElementRef<"button">, ComponentPropsWithoutRef<"button">>(
+  ({ className, ...props }, ref) => (
+    <button
+      ref={ref}
+      className={cn(
+        "py-[8.5px] mr-[113px] text-gray-600 bg-white text-left",
+        textVariants({ variant: "body/15_m" }),
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+Item.displayName = "DialogItem";
+
+export const DialogMenu = () => {
+  return (
+    <DialogContent.Content>
+      <Stack className="px-4 py-2">
+        <Item>메모 수정하기</Item>
+        <Item>메모 삭제하기</Item>
+      </Stack>
+    </DialogContent.Content>
+  );
+};
