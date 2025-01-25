87d99e97af48a0d1ae9085e4754b4da81d1e7efa
devgiljong <calf-love@naver.com>
2025-01-25 15:34:43 +0900

    fix: line

--- src/features/bookRecordRead/components/FirstUserRecordFallback.tsx
+++ src/features/bookRecordRead/components/FirstUserRecordFallback.tsx
@@ -5,19 +5,22 @@ import { Text } from "@repo/design-system/Text";
 import { CenterStack } from "@repo/ui/CenterStack";
 
 import { Spacing } from "@repo/ui/Spacing";
+import { Stack } from "@repo/ui/Stack";
 
 export const FirstUserRecordFallback = () => {
   return (
-    <CenterStack className="h-full px-[72px]">
-      <AspectRatio ratio={283 / 210}>
-        <Image src={EMPTY_FALLBACK} alt="list empty fallback image" objectfit={"fill"} />
-      </AspectRatio>
-      <Spacing className=" h-[128px]" />
+    <Stack className=" w-full h-full">
+      <CenterStack className="h-full w-full px-[72px]">
+        <AspectRatio ratio={283 / 210}>
+          <Image src={EMPTY_FALLBACK} alt="list empty fallback image" objectfit={"fill"} />
+        </AspectRatio>
+        <Spacing className=" h-[128px]" />
+      </CenterStack>
       <Text
         variant={"body/18_m"}
         className=" text-gray-1000 text-center line-clamp-2 whitespace-pre-wrap"
       >{`나의 독서 습관을 만들어줄\n첫 번째 독서기록을 시작해보세요`}</Text>
-    </CenterStack>
+    </Stack>
   );
 };
 
