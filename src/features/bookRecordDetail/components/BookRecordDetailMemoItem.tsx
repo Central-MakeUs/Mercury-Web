384f46bd67f632c7eabf222ee6a01bc4da649cd2
Yujin <114130942+Youjiiin@users.noreply.github.com>
2025-01-25 14:44:45 +0900

    fix: 메모 한줄시 디자인 변경 (#68)

--- src/features/bookRecordDetail/components/BookRecordDetailMemoItem.tsx
+++ src/features/bookRecordDetail/components/BookRecordDetailMemoItem.tsx
@@ -5,7 +5,7 @@ import { Stack } from "@repo/ui/Stack";
 
 import { Pressable } from "@repo/design-system/Pressable";
 import { cn } from "@repo/design-system/cn";
-import type { PropsWithChildren, ReactNode } from "react";
+import { type PropsWithChildren, type ReactNode, useEffect, useRef, useState } from "react";
 
 export const BookRecordDetailMemoItem = (
   props: PropsWithChildren<{
@@ -14,6 +14,15 @@ export const BookRecordDetailMemoItem = (
   }>,
 ) => {
   const { header, children, onPressComplete = () => {} } = props;
+  const memoRef = useRef<HTMLDivElement>(null);
+  const [boxMarginTop, setBoxMarginTop] = useState("top-[13px]");
+  useEffect(() => {
+    if (memoRef.current) {
+      const memoHeight = memoRef.current.clientHeight;
+      setBoxMarginTop(memoHeight < 44 ? "top-[2px]" : "top-[13px]");
+    }
+  }, []);
+
   return (
     <Stack className="">
       <Flex className={cn(" text-gray-600 mb-2 ", textVariants({ variant: "body/13_r" }))}>
@@ -21,12 +30,15 @@ export const BookRecordDetailMemoItem = (
       </Flex>
       <Pressable onPressComplete={onPressComplete} delay={600}>
         <Stack className=" touch-none select-none w-full pl-4  transition-all data-[pressed=true]:duration-[400ms] data-[pressed=true]:scale-[0.98] data-[pressed=true]:opacity-70 data-[pressed=false]:duration-[400ms]">
-          <Stack className=" p-[10px] min-h-[66px] relative bg-yellow-green w-full rounded-[12px]">
+          <Stack
+            ref={memoRef}
+            className=" p-[10px] min-h-[43px] relative bg-yellow-green w-full rounded-[12px]"
+          >
             <Text className=" w-full text-gray-600" variant={"body/15_r"}>
               {children}
             </Text>
             <Image
-              className=" absolute top-[13px] left-[-8px] z-[-1]"
+              className={cn(" absolute left-[-8px] z-[-1]", boxMarginTop)}
               src="/images/common/polygon_yellow_green.webp"
               alt="polygon"
             />
