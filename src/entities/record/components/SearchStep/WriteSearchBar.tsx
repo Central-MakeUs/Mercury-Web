70bd46d7010cce4f0097db5dc7e0ae0d4135a612
devgiljong <calf-love@naver.com>
2025-01-26 22:02:06 +0900

    fix: 디바운싱 풀리는 오류 수정

--- src/entities/record/components/SearchStep/WriteSearchBar.tsx
+++ src/entities/record/components/SearchStep/WriteSearchBar.tsx
@@ -1,23 +1,22 @@
 import { SearchBar } from "@repo/design-system/SearchBar";
 import { MagnifyIcon } from "@repo/icon/MagnifyIcon";
-import { useDebouncedInputValue } from "@xionwcfm/react";
-import { useEffect } from "react";
+import { useDebounce } from "@xionwcfm/react";
+import { useState } from "react";
 import { useWriteSearchStore } from "./WriteSearchStep.store";
 
 export const WriteSearchBar = () => {
   const storeValue = useWriteSearchStore((state) => state.query);
   const setQuery = useWriteSearchStore((state) => state.actions.setQuery);
-  const debouncing = useDebouncedInputValue(storeValue, { delay: 2000 });
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-  useEffect(() => {
-    setQuery(debouncing.debouncedValue);
-  }, [debouncing.debouncedValue]);
+  const [value, setvalue] = useState(storeValue);
+  const debouncedQuery = useDebounce(setQuery, 2000);
 
   return (
     <SearchBar
-      value={debouncing.value}
-      onChange={(e) => debouncing.onChange(e.target.value)}
+      value={value}
+      onChange={(e) => {
+        setvalue(e.target.value);
+        debouncedQuery(e.target.value);
+      }}
       left={<MagnifyIcon />}
       placeholder="책 제목을 검색해주세요"
     />
