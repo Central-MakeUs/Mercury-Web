5ac87846e261df126a5acce19b21168c8d997c76
devgiljong <calf-love@naver.com>
2025-02-05 23:51:07 +0900

    refactor: setting page 리팩토링

--- src/entities/user/components/LogoutDialog.tsx
+++ src/entities/user/components/LogoutDialog.tsx
@@ -1,66 +1,12 @@
 import { Button } from "@repo/design-system/Button";
 import { Dialog } from "@repo/design-system/Dialog";
 import { MaxWidthBox } from "@repo/design-system/MaxWidthBox";
-import { Text, textVariants } from "@repo/design-system/Text";
-import { cn } from "@repo/design-system/cn";
-import { Flex } from "@repo/ui/Flex";
+import { Text } from "@repo/design-system/Text";
 import { JustifyBetween } from "@repo/ui/JustifyBetween";
 import { Stack } from "@repo/ui/Stack";
 import { overlay } from "overlay-kit";
-import { useState } from "react";
-import { openExternalUrl } from "~/shared/utils/openExternalUrl";
 
-interface SettingMenuItemProps {
-  type: "toggle" | "exit" | "link";
-  menuName: string;
-  onClick?: () => void;
-  link?: string;
-  defaultValue?: boolean;
-}
-
-export const SettingMenuItem = ({
-  type,
-  menuName,
-  onClick,
-  link,
-  defaultValue,
-}: SettingMenuItemProps) => {
-  const [isOn, setIsOn] = useState(defaultValue || false);
-
-  const handleClick = () => {
-    if (type === "toggle") {
-      setIsOn(!isOn);
-    } else if (type === "exit") {
-      _LogoutOverlay.open();
-    } else if (type === "link" && link) {
-      openExternalUrl(link);
-    }
-  };
-
-  return (
-    <Flex className="py-4 flex justify-between items-center cursor-pointer" onClick={handleClick}>
-      <Text
-        variant="body/16_sb"
-        className={cn(type === "exit" ? "text-gray-400" : "text-gray-800")}
-      >
-        {menuName}
-      </Text>
-      {type === "toggle" && (
-        <button
-          onClick={handleClick}
-          className={cn(
-            textVariants({ variant: "body/16_sb" }),
-            isOn ? "text-green" : "text-gray-500",
-          )}
-        >
-          {isOn ? "ON" : "OFF"}
-        </button>
-      )}
-    </Flex>
-  );
-};
-
-const _LogoutOverlay = {
+export const logoutDialogOverlay = {
   open: () => {
     return overlay.open(({ isOpen, close, unmount }) => {
       return (
