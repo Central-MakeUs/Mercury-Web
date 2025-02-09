fb985e02b0f917ca6aa6a5965d6baf42977adea0
Youjiiin <yujinjang24@gmail.com>
2025-02-09 16:55:48 +0900

    feat: 설정정url 수정 및 게이지 디자인 수정

--- src/shared/constants/externalLink.ts
+++ src/shared/constants/externalLink.ts
@@ -1,5 +1,5 @@
 export const SETTINGS_LINKS = {
-  FAQ: "https://burly-jar-165.notion.site/18828ba3580d80f8b23ef2a7082f8412?pvs=4",
-  TERMSANDPRIVACY: "https://burly-jar-165.notion.site/use-policy?pvs=4",
-  NOTICE: "https://burly-jar-165.notion.site/announcement?pvs=4",
+  FAQ: "http://mercuryplanet.notion.site",
+  TERMSANDPRIVACY: "https://mercuryplanet.notion.site/use-policy",
+  NOTICE: "https://mercuryplanet.notion.site/announcement",
 };
