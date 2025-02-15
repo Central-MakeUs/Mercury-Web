184ca72fa7faab851d8f9b5bcb86346e1c94af75
devgiljong <calf-love@naver.com>
2025-02-15 23:55:45 +0900

    chore: fallback add

--- src/entities/user/components/Signed.tsx
+++ src/entities/user/components/Signed.tsx
@@ -1,12 +1,12 @@
 import type { PropsWithChildren } from "react";
 import { authStore } from "../model/auth.store";
 
-export const Signed = (props: PropsWithChildren) => {
+export const Signed = (props: PropsWithChildren<{ fallback?: React.ReactNode }>) => {
   const auth = authStore.useAuth();
   const isLoggedIn = auth.isLoggedIn;
 
   if (!isLoggedIn) {
-    return null;
+    return props.fallback ?? null;
   }
 
   return props.children;
