372b69a9aee3c08007eab33e0ad6a37e1e97614f
devgiljong <calf-love@naver.com>
2025-02-16 01:20:07 +0900

    feat: page capture add

--- packages/analytics/index.tsx
+++ packages/analytics/index.tsx
@@ -1,7 +1,9 @@
 import Clarity from "@microsoft/clarity";
 import { env } from "@repo/env";
+import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { type PropsWithChildren, useEffect } from "react";
+import { useLocation, useSearchParams } from "react-router";
 
 export const Analytics = () => {
   return (
@@ -22,16 +24,41 @@ const ClarityEffector = () => {
   return null;
 };
 
+const PostHogPageView = () => {
+  const location = useLocation();
+  const [searchParams] = useSearchParams();
+  const pathname = location.pathname;
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    if (pathname && posthog) {
+      let url = window.origin + pathname;
+      if (searchParams.toString()) {
+        url += `?${searchParams.toString()}`;
+      }
+      posthog.capture("$pageview", { $current_url: url });
+    }
+  }, [pathname, searchParams, posthog]);
+  return null;
+};
+
 export const MercuryPostHogProvider = (props: PropsWithChildren) => {
   const apiKey = import.meta.env.VITE_PUBLIC_POSTHOG_KEY ?? null;
   const apiHost = import.meta.env.VITE_PUBLIC_POSTHOG_HOST ?? null;
+
+  useEffect(() => {
+    if (apiKey && apiHost) {
+      posthog.init(apiKey, { api_host: apiHost });
+    }
+  }, []);
 
   if (!(apiKey && apiHost)) {
     return <>{props.children}</>;
   }
 
   return (
-    <PostHogProvider apiKey={apiKey} options={{ api_host: apiHost }}>
+    <PostHogProvider client={posthog}>
+      <PostHogPageView />
       {props.children}
     </PostHogProvider>
   );
