0107253d3efc0d3ddfc33b5651cae74ee9866eca
YuGilJong <101111364+XionWCFM@users.noreply.github.com>
2025-02-17 21:36:05 +0900

    feat: 리프레쉬토큰 추가 (#98)

--- src/entities/user/model/auth.store.ts
+++ src/entities/user/model/auth.store.ts
@@ -3,14 +3,24 @@ import { useLocalStorageService } from "@repo/storage/useLocalStorageService";
 import { useCallback } from "react";
 
 export const AUTH_ACCESS_TOKEN_STORAGE_KEY = "@mercury_authentication";
-
+export const AUTH_REFRESH_TOKEN_STORAGE_KEY = "@mercury_refresh_token";
 export const authStore = {
+  setRefreshToken: (refreshToken: string | null) => {
+    LocalStorageService.setItem(AUTH_REFRESH_TOKEN_STORAGE_KEY, refreshToken);
+  },
+
+  getRefreshToken: () => {
+    return LocalStorageService.getItem(AUTH_REFRESH_TOKEN_STORAGE_KEY);
+  },
+
   setAccessToken: (accessToken: string | null) => {
     LocalStorageService.setItem(AUTH_ACCESS_TOKEN_STORAGE_KEY, accessToken);
   },
+
   getAccessToken: () => {
     return LocalStorageService.getItem(AUTH_ACCESS_TOKEN_STORAGE_KEY);
   },
+
   isLoggedIn: () => {
     return authStore.getAccessToken() !== null;
   },
@@ -23,7 +33,15 @@ export const authStore = {
       },
     );
 
+    const [refreshToken, _setRefreshToken] = useLocalStorageService<null | string>(
+      AUTH_REFRESH_TOKEN_STORAGE_KEY,
+      {
+        defaultValue: null,
+      },
+    );
+
     const isLoggedIn = accessToken !== null;
+
     const setAccessToken = useCallback(
       (accessToken: string | null) => {
         setValue(accessToken);
@@ -31,10 +49,19 @@ export const authStore = {
       [setValue],
     );
 
+    const setRefreshToken = useCallback(
+      (refreshToken: string | null) => {
+        _setRefreshToken(refreshToken);
+      },
+      [_setRefreshToken],
+    );
+
     return {
       isLoggedIn,
       accessToken,
       setAccessToken,
+      refreshToken,
+      setRefreshToken,
     };
   },
 };
