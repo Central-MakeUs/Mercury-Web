811702214c14b33f013d3050e2d97486ed326c5f
devgiljong <calf-love@naver.com>
2025-02-19 00:33:02 +0900

    fix: build

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -5,6 +5,8 @@ export const env = createEnv({
   client: {
     VITE_API_URL: z.string().min(1),
     VITE_PUBLIC_DEEP_LINK_SCHEME: z.string().min(1),
+    VITE_PUBLIC_POSTHOG_KEY: z.string().min(1),
+    VITE_PUBLIC_POSTHOG_HOST: z.string().min(1),
   },
   shared: {
     DEV: z.boolean(),
@@ -17,5 +19,7 @@ export const env = createEnv({
     DEV: import.meta.env.DEV,
     PROD: import.meta.env.PROD,
     VITE_PUBLIC_DEEP_LINK_SCHEME: import.meta.env.VITE_PUBLIC_DEEP_LINK_SCHEME,
+    VITE_PUBLIC_POSTHOG_KEY: import.meta.env.VITE_PUBLIC_POSTHOG_KEY,
+    VITE_PUBLIC_POSTHOG_HOST: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   },
 });
