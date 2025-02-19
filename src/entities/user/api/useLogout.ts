3e7b783165c8893c148c5666cc1695fd8db72b60
Youjiiin <yujinjang24@gmail.com>
2025-02-19 17:03:29 +0900

    feat: 로그아웃 에러처리

--- src/entities/user/api/useLogout.ts
+++ src/entities/user/api/useLogout.ts
@@ -7,9 +7,14 @@ export const useLogout = () => {
   const [isLoading, setLoading] = useLoading();
   const auth = authStore.useAuth();
   const logout = useCallback(async () => {
-    await setLoading(http.post("/signoff/logout"));
-    auth.setAccessToken(null);
-    auth.setRefreshToken(null);
+    try {
+      await setLoading(http.post("/signoff/logout"));
+    } catch (error) {
+      console.error("로그아웃 실패:", error);
+    } finally {
+      auth.setAccessToken(null);
+      auth.setRefreshToken(null);
+    }
   }, [setLoading, auth]);
   return { logout, isLoading };
 };
