c19a125a6d91d013614b0fab479b4f8f2c74d0fb
Youjiiin <yujinjang24@gmail.com>
2025-02-27 21:15:10 +0900

    feat: 온보딩 배경색

--- src/pages/FirstOnBoardingPage.tsx
+++ src/pages/FirstOnBoardingPage.tsx
@@ -1,3 +1,4 @@
+import { SafeArea } from "@repo/bridge-web/SafeArea";
 import { FixedBottom } from "@repo/design-system/FixedBottom";
 import { Text } from "@repo/design-system/Text";
 import { TopNavigation } from "@repo/design-system/TopNavigation";
@@ -61,53 +62,55 @@ const FirstOnBoardingPage = () => {
   const CurrentStepComponent = steps[currentStep];
 
   return (
-    <Stack
-      className="w-full h-full items-center justify-center bg-navy overflow-hidden"
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-    >
-      <TopNavigation.Root
-        right={
-          <Link to={"/onboarding"}>
-            <Text variant={"body/15_m"} className="text-gray-300 mr-[25px]">
-              skip
-            </Text>
-          </Link>
-        }
-        className="w-full"
+    <SafeArea edges={["bottom"]}>
+      <Stack
+        className="w-full h-full items-center justify-center bg-navy overflow-hidden"
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
       >
-        <TopNavigation.Title className="sr-only">머큐리 사용 설명</TopNavigation.Title>
-      </TopNavigation.Root>
-      <Flex className="relative w-full h-full overflow-hidden flex-col justify-center items-center">
-        <AnimatePresence initial={false} custom={direction}>
-          <motion.div
-            key={currentStep}
-            initial={{ x: direction === 1 ? "100%" : "-100%", opacity: 0 }}
-            animate={{ x: "0%", opacity: 1 }}
-            exit={{ x: direction === 1 ? "-100%" : "100%", opacity: 0 }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
-            className="relative w-full h-full flex items-center justify-center"
-          >
-            <CurrentStepComponent />
-          </motion.div>
-        </AnimatePresence>
-      </Flex>
-      <FixedBottom className="w-full pt-[29px] pb-[52px] bg-gradient-to-r from-main3-gradient-from to-main4-gradient-to rounded-t-[20px]">
-        <Flex className="w-full py-4 flex justify-center items-center">
-          {[...Array(5)].map((_, index) => (
-            <div
-              key={index}
-              className={`mx-1 transition-all ${
-                (currentStep === index && index < 4) || (index === 4 && currentStep === 4)
-                  ? "w-[10px] h-[6px] bg-white"
-                  : "w-[6px] h-[6px] bg-gray-200"
-              } rounded-full`}
-            />
-          ))}
+        <TopNavigation.Root
+          right={
+            <Link to={"/onboarding"}>
+              <Text variant={"body/15_m"} className="text-gray-300 mr-[25px]">
+                skip
+              </Text>
+            </Link>
+          }
+          className="w-full"
+        >
+          <TopNavigation.Title className="sr-only">머큐리 사용 설명</TopNavigation.Title>
+        </TopNavigation.Root>
+        <Flex className="relative w-full h-full overflow-hidden flex-col justify-center items-center bg-navy ">
+          <AnimatePresence initial={false} custom={direction}>
+            <motion.div
+              key={currentStep}
+              initial={{ x: direction === 1 ? "100%" : "-100%", opacity: 0 }}
+              animate={{ x: "0%", opacity: 1 }}
+              exit={{ x: direction === 1 ? "-100%" : "100%", opacity: 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="relative w-full h-full flex items-center justify-center"
+            >
+              <CurrentStepComponent />
+            </motion.div>
+          </AnimatePresence>
         </Flex>
-      </FixedBottom>
-    </Stack>
+        <FixedBottom className="w-full pt-[29px] pb-[52px] bg-gradient-to-r from-main3-gradient-from to-main4-gradient-to rounded-t-[20px]">
+          <Flex className="w-full py-4 flex justify-center items-center">
+            {[...Array(5)].map((_, index) => (
+              <div
+                key={index}
+                className={`mx-1 transition-all ${
+                  (currentStep === index && index < 4) || (index === 4 && currentStep === 4)
+                    ? "w-[10px] h-[6px] bg-white"
+                    : "w-[6px] h-[6px] bg-gray-200"
+                } rounded-full`}
+              />
+            ))}
+          </Flex>
+        </FixedBottom>
+      </Stack>
+    </SafeArea>
   );
 };
 
