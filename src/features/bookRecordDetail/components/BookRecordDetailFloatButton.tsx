d06772620b55d5f3f839a375ac384dc592d8f6a7
Youjiiin <yujinjang24@gmail.com>
2025-03-03 17:57:55 +0900

    fix: memoDetail에서 뒤로가기 커스텀

--- src/features/bookRecordDetail/components/BookRecordDetailFloatButton.tsx
+++ src/features/bookRecordDetail/components/BookRecordDetailFloatButton.tsx
@@ -2,17 +2,18 @@ import { SafeArea } from "@repo/bridge-web/SafeArea";
 import { FloatingActionButton } from "@repo/design-system/FloatingActionButton";
 import { MaxWidthBox } from "@repo/design-system/MaxWidthBox";
 import { cn } from "@repo/design-system/cn";
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 export const BookRecordDetailFloatButton = (props: { className?: string; recordId: string }) => {
   const { className, recordId } = props;
+  const navigate = useNavigate();
   return (
     <MaxWidthBox className={cn(" fixed bottom-0 left-[50%] translate-x-[-50%]", className)}>
       <MaxWidthBox className="flex justify-end mb-[94px] pr-[18px]">
         <SafeArea edges={["bottom"]}>
-          <Link to={`/add-memo/${recordId}`}>
+          <button onClick={() => navigate(`/add-memo/${recordId}`, { replace: true })}>
             <FloatingActionButton variant={"plus"} />
-          </Link>
+          </button>
         </SafeArea>
       </MaxWidthBox>
     </MaxWidthBox>
