c5e4233bf26e369dec1801ec2df49d459efa0e7d
Youjiiin <yujinjang24@gmail.com>
2025-03-05 16:37:27 +0900

    chore: 온보딩 화면 사진 이름 변경

--- src/shared/images/onboarding/onboardingImages.ts
+++ src/shared/images/onboarding/onboardingImages.ts
@@ -1,7 +1,7 @@
 export const ONBOARDING_ASSETS = {
-  ONBOARDING1_WEBP: "/images/onboarding/onBoarding1.webp",
-  ONBOARDING2_WEBP: "/images/onboarding/onBoarding2.webp",
-  ONBOARDING3_WEBP: "/images/onboarding/onBoarding3.webp",
-  ONBOARDING4_WEBP: "/images/onboarding/onBoarding4.webp",
+  ONBOARDING1_WEBP: "/images/onboarding/onBoardingFunnel1.webp",
+  ONBOARDING2_WEBP: "/images/onboarding/onBoardingFunnel2.webp",
+  ONBOARDING3_WEBP: "/images/onboarding/onBoardingFunnel3.webp",
+  ONBOARDING4_WEBP: "/images/onboarding/onBoardingFunnel4.webp",
   ONBOARDINGBACK_WEBP: "/images/onboarding/onBoardingBack.webp",
 };
