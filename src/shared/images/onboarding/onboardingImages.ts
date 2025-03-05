86e7d352ca3e0eb674a816d47bc8ea7f05a42bf2
Youjiiin <yujinjang24@gmail.com>
2025-03-05 16:23:43 +0900

    chore: 온보딩 이미지 정리

--- src/shared/images/onboarding/onboardingImages.ts
+++ src/shared/images/onboarding/onboardingImages.ts
@@ -1,8 +1,4 @@
 export const ONBOARDING_ASSETS = {
-  FIRSTONBOARDING1_WEBP: "/images/onboarding/firstOnBoarding1.webp",
-  FIRSTONBOARDING2_WEBP: "/images/onboarding/firstOnBoarding2.webp",
-  FIRSTONBOARDING3_WEBP: "/images/onboarding/firstOnBoarding3.webp",
-  FIRSTONBOARDING4_WEBP: "/images/onboarding/firstOnBoarding4.webp",
   ONBOARDING1_WEBP: "/images/onboarding/onBoarding1.webp",
   ONBOARDING2_WEBP: "/images/onboarding/onBoarding2.webp",
   ONBOARDING3_WEBP: "/images/onboarding/onBoarding3.webp",
