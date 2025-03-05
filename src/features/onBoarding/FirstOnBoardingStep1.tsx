4bd1b6fd49725da4ee21dae69732a8b005853fa4
Yujin <114130942+Youjiiin@users.noreply.github.com>
2025-03-05 16:15:36 +0900

    fix: 온보딩 레이아웃 수정 (#143)
    * feat: 온보딩 퍼널
    
    * fix: motion의존성 중복 제거
    
    * feat: 온보딩 다시보기
    
    * feat: 온보딩 다시보기
    
    * feat: 온보딩 배경색
    
    * fix: 온보딩 퍼널 safeArea 수정
    
    * fix: 온보딩 퍼널 safeArea 수정
    
    * feat: 온보딩 퍼널 분리
    
    * feat: 온보딩 퍼널 이미지 aspectratio
    
    * fix: 온보딩 퍼널 QA 반영
    
    * fix: navigate위치 수정
    
    * fix: 게이지 이미지 수정
    
    * fix: 독서기록 로그인 시 렌더링 오류 해결
    
    * fix: 메모 detail 접속 오류 수정
    
    * fix: 메모 작성 페이지 뷰 어색함 해결
    
    * fix: 메모 줄바꿈 적용
    
    * fix: memoDetail에서 뒤로가기 커스텀
    
    * fix: 타이머 재설정 시, 일시정지 후 바텀시트 오픈
    
    * feat: memo입력 디자인 변경
    
    * fix: 잃어버린 textCount 복구
    
    * fix: 잃어버린 textCount 복구
    
    * fix: 독서기록 빈페이지 글자 잘리는 문제
    
    * feat: habitbar클릭시에도 오늘 스트릭 확인
    
    * fix: 온보딩 레이아웃 수정

--- src/features/onBoarding/FirstOnBoardingStep1.tsx
+++ src/features/onBoarding/FirstOnBoardingStep1.tsx
@@ -1,37 +1,35 @@
-import { AspectRatio } from "@repo/design-system/AspectRatio";
 import { Image } from "@repo/design-system/Image";
 import { Text } from "@repo/design-system/Text";
 import { CenterStack } from "@repo/ui/CenterStack";
 import { Stack } from "@repo/ui/Stack";
+import { ONBOARDING_ASSETS } from "~/shared/images/onboarding/onboardingImages";
 
 export const FirstOnBoardingStep1 = () => {
   return (
-    <Stack className="w-screen max-w-[450px] bg-navy ">
-      <Stack className="relative">
-        <CenterStack className="gap-[10px] w-full z-[2] mt-10 absolute top-0">
-          <Text
-            variant={"body/16_m"}
-            className="text-gray-100 whitespace-pre-wrap opacity-80 text-center"
-          >{`책을 읽으면서 들었던 생각을\n가볍게 메모로 남겨보세요`}</Text>
-          <Text variant={"title/25_b"} className="text-white text-[29px]">
-            간단한 독서기록
-          </Text>
-        </CenterStack>
+    <Stack className="w-screen h-screen max-w-[450px] bg-navy ">
+      <CenterStack className="gap-[10px] w-full mt-10">
+        <Text
+          variant={"body/16_m"}
+          className="text-gray-100 whitespace-pre-wrap opacity-80 text-center"
+        >{`책을 읽으면서 들었던 생각을\n가볍게 메모로 남겨보세요`}</Text>
 
-        <AspectRatio ratio={375 / 800} className="z-[2] absolute">
-          <Image
-            src={"/images/onboarding/onBoarding1.webp"}
-            alt="onBoardingFunnel1"
-            objectfit={"cover"}
-            className="z-[2] absolute"
-          />
-        </AspectRatio>
-      </Stack>
+        <Text variant={"title/25_b"} className="text-white text-[29px]">
+          간단한 독서기록
+        </Text>
+      </CenterStack>
+
       <Image
-        src={"/images/onboarding/onBoardingBack.webp"}
-        alt="onBoardingFunnel2"
+        src={ONBOARDING_ASSETS.ONBOARDING1_WEBP}
+        alt="onBoardingFunnel1"
         objectfit={"cover"}
-        className="absolute inset-0 z-[1]"
+        className="absolute z-[2]"
+      />
+
+      <Image
+        src={ONBOARDING_ASSETS.ONBOARDINGBACK_WEBP}
+        alt="onBoardingFunnelBack"
+        objectfit={"cover"}
+        className="absolute z-[1]"
       />
     </Stack>
   );
--- src/features/onBoarding/FirstOnBoardingStep3.tsx
+++ src/features/onBoarding/FirstOnBoardingStep3.tsx
@@ -1,37 +1,34 @@
-import { AspectRatio } from "@repo/design-system/AspectRatio";
 import { Image } from "@repo/design-system/Image";
 import { Text } from "@repo/design-system/Text";
 import { CenterStack } from "@repo/ui/CenterStack";
 import { Stack } from "@repo/ui/Stack";
+import { ONBOARDING_ASSETS } from "~/shared/images/onboarding/onboardingImages";
 
 export const FirstOnBoardingStep3 = () => {
   return (
-    <Stack className="w-screen max-w-[450px] bg-navy">
-      <Stack className="relative">
-        <CenterStack className="gap-[10px] w-full z-[2] mt-10 absolute top-0">
-          <Text
-            variant={"body/16_m"}
-            className="text-gray-100 whitespace-pre-wrap opacity-80 text-center"
-          >{`꾸준히 독서기록을 쌓으며\n책 읽는 습관을 들일 수 있어요`}</Text>
-          <Text variant={"title/25_b"} className="text-white text-[29px]">
-            쉽게 만드는 독서 습관
-          </Text>
-        </CenterStack>
-
-        <AspectRatio ratio={375 / 800} className="z-[2] absolute">
-          <Image
-            src={"/images/onboarding/onBoarding3.webp"}
-            alt="onBoardingFunnel1"
-            objectfit={"cover"}
-          />
-        </AspectRatio>
-      </Stack>
+    <Stack className="w-screen h-screen max-w-[450px] bg-navy ">
+      <CenterStack className="gap-[10px] w-full mt-10">
+        <Text
+          variant={"body/16_m"}
+          className="text-gray-100 whitespace-pre-wrap opacity-80 text-center"
+        >{`꾸준히 독서기록을 쌓으며\n책 읽는 습관을 들일 수 있어요`}</Text>
+        <Text variant={"title/25_b"} className="text-white text-[29px]">
+          쉽게 만드는 독서 습관
+        </Text>
+      </CenterStack>
 
       <Image
-        src={"/images/onboarding/onBoardingBack.webp"}
-        alt="onBoardingFunnel2"
+        src={ONBOARDING_ASSETS.ONBOARDING3_WEBP}
+        alt="onBoardingFunnel3"
         objectfit={"cover"}
-        className="absolute inset-0 z-[1]"
+        className="absolute z-[2]"
+      />
+
+      <Image
+        src={ONBOARDING_ASSETS.ONBOARDINGBACK_WEBP}
+        alt="onBoardingFunnelBack"
+        objectfit={"cover"}
+        className="absolute z-[1]"
       />
     </Stack>
   );
