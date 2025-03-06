d9fea4866be6ddd42f603b6632d7b46304bbc725
Youjiiin <yujinjang24@gmail.com>
2025-03-06 23:15:52 +0900

    chore: 온보딩 파일명 변경

--- src/features/onBoarding/FirstOnBoardingStep4.tsx
+++ src/features/onBoarding/FirstOnBoardingStep4.tsx
@@ -1,14 +1,13 @@
 import { AspectRatio } from "@repo/design-system/AspectRatio";
 import { Image } from "@repo/design-system/Image";
 import { Stack } from "@repo/ui/Stack";
-import { ONBOARDING_ASSETS } from "~/shared/images/onboarding/onboardingImages";
 
 export const FirstOnBoardingStep4 = () => {
   return (
     <Stack className="w-screen max-w-[450px] h-full">
       <AspectRatio ratio={375 / 800}>
         <Image
-          src={ONBOARDING_ASSETS.ONBOARDING4_WEBP}
+          src="/images/onboarding/onBoardingFunnel4.webp"
           alt="onBoardingFunnel2"
           objectfit={"cover"}
           className="absolute bottom-[100px]"
