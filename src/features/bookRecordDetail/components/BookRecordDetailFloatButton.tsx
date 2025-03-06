d76750500a3611fb1849594d6883ead3e6b28652
Youjiiin <yujinjang24@gmail.com>
2025-03-06 22:55:42 +0900

    fix: 플로팅 버튼 위치 수정

--- src/features/bookRecordDetail/components/BookRecordDetailFloatButton.tsx
+++ src/features/bookRecordDetail/components/BookRecordDetailFloatButton.tsx
@@ -1,4 +1,3 @@
-import { SafeArea } from "@repo/bridge-web/SafeArea";
 import { FloatingActionButton } from "@repo/design-system/FloatingActionButton";
 import { MaxWidthBox } from "@repo/design-system/MaxWidthBox";
 import { cn } from "@repo/design-system/cn";
@@ -8,14 +7,10 @@ export const BookRecordDetailFloatButton = (props: { className?: string; recordI
   const { className, recordId } = props;
   const navigate = useNavigate();
   return (
-    <MaxWidthBox className={cn(" fixed bottom-0 left-[50%] translate-x-[-50%]", className)}>
-      <MaxWidthBox className="flex justify-end mb-[94px] pr-[18px]">
-        <SafeArea edges={["bottom"]}>
-          <div onClick={() => navigate(`/add-memo/${recordId}`, { replace: true })}>
-            <FloatingActionButton variant={"plus"} />
-          </div>
-        </SafeArea>
-      </MaxWidthBox>
+    <MaxWidthBox className={cn(" fixed bottom-[113px] left-[80%] ", className)}>
+      <div onClick={() => navigate(`/add-memo/${recordId}`, { replace: true })}>
+        <FloatingActionButton variant={"plus"} />
+      </div>
     </MaxWidthBox>
   );
 };
--- src/features/bookRecordRead/GoBookRecordWriteButton.tsx
+++ src/features/bookRecordRead/GoBookRecordWriteButton.tsx
@@ -1,20 +1,14 @@
-import { Link } from "react-router";
-
-import { SafeArea } from "@repo/bridge-web/SafeArea";
 import { FloatingActionButton } from "@repo/design-system/FloatingActionButton";
 import { MaxWidthBox } from "@repo/design-system/MaxWidthBox";
-import { Box } from "@repo/ui/Box";
+import { cn } from "@repo/design-system/cn";
+import { Link } from "react-router";
 
 export const GoBookRecordWriteButton = () => {
   return (
-    <Box className=" fixed bottom-0 left-[50%] translate-x-[-50%]">
-      <MaxWidthBox className="flex justify-end mb-[94px] pr-[18px]">
-        <SafeArea edges={["bottom"]}>
-          <Link to={"/book-record/write"}>
-            <FloatingActionButton />
-          </Link>
-        </SafeArea>
-      </MaxWidthBox>
-    </Box>
+    <MaxWidthBox className={cn(" fixed bottom-[113px] left-[80%] ")}>
+      <Link to={"/book-record/write"}>
+        <FloatingActionButton />
+      </Link>
+    </MaxWidthBox>
   );
 };
