7c5b1268fee6ca753b0439201f777a06f057b24c
Youjiiin <yujinjang24@gmail.com>
2025-03-06 16:22:07 +0900

    style: 게이지 디자인 오류 해결

--- packages/design-system/SingleSlider.tsx
+++ packages/design-system/SingleSlider.tsx
@@ -1,7 +1,7 @@
 import * as SliderPrimitive from "@radix-ui/react-slider";
+import { cn } from "@repo/design-system/cn";
 import { Box } from "@repo/ui/Box";
 import type { ComponentProps } from "react";
-import { cn } from "./cn";
 
 export const SingleSlider = (
   props: Omit<
@@ -20,12 +20,17 @@ export const SingleSlider = (
       value={value ? [value] : undefined}
       onValueChange={(value) => onChange?.(value[0])}
       className={cn(
-        "relative w-full flex touch-none select-none  items-center h-[48px] grow rounded-full ",
+        "relative w-full flex touch-none select-none  items-center h-[48px] grow ",
         className,
       )}
     >
       <SliderPrimitive.Track className="bg-gray-200 relative rounded-full w-full h-[24px]">
-        <SliderPrimitive.Range className="absolute bg-gradient-to-r from-main2-gradient-from via-main2-gradient-via to-main2-gradient-to rounded-full h-full" />
+        <SliderPrimitive.Range
+          className={cn(
+            "absolute bg-gradient-to-r from-main2-gradient-from via-main2-gradient-via to-main2-gradient-to h-full min-w-[30px]",
+            value && value < 30 ? "rounded-l-full rounded-r-none" : "rounded-full",
+          )}
+        />
       </SliderPrimitive.Track>
       <SliderPrimitive.Thumb className=" duration-300 transition-transform active:scale-105 flex justify-center items-center focus:outline-none size-[48px] rounded-full bg-pastel-red bg-gradient-to-tl from-green to-yellow-green">
         <Box className=" size-[40px] rounded-full bg-green flex justify-center items-center">
